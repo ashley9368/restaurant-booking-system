3a3104ff674803abf7e4208ea98ff1df05cd4900
Ashley Goward <ashleygoward@icloud.com>
2024-12-08 10:03:28 +0000

    add core and link urls

--- restaurant_booking_system/urls.py
+++ restaurant_booking_system/urls.py
@@ -19,4 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),            # Core app
+    path('bookings/', include('bookings.urls')),  # Bookings app
 ]
