a8594f17958247cede561667c37df41b63258f0e
Ashley Goward <ashleygoward@icloud.com>
2024-12-08 14:08:18 +0000

    Add menu that opens and closes at the top left of the home page

---
+++ core/static/core/script.js
@@ -0,0 +1,8 @@
+// This function toggles the visibility of the dropdown menu when the button is clicked
+
+function toggleMenu() {
+    var menu = document.getElementById("dropdownList");
+
+    // Toggle the 'visible' class to show or hide the menu
+    menu.classList.toggle("visible");
+};
