d4e9169435726694bd26db29d404b97e62b7d26c
Ashley Goward <ashleygoward@icloud.com>
2024-12-09 11:00:55 +0000

    Remove core app, move html, js and css to root

--- restaurant_booking_system/settings.py
+++ restaurant_booking_system/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s+f=n)umjjg!96-o)pjr$irh^dm%9ld3*)yn=7mi!k0&ye946+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ashley9368-restaurantbo-ea4vxr0qerx.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ashley9368-restaurantbo-ea4vxr0qerx.ws.codeinstitute-ide.net', 
+'.herokuapp.com']
 
 
 # Application definition
