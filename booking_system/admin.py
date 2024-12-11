6c40e9a0528124616fe86775711607557b383218
Ashley Goward <ashleygoward@icloud.com>
2024-12-11 11:47:54 +0000

    Add TableBooking model and register it in Django admin, add CRSF TRUSTED ORIGINS

--- booking_system/admin.py
+++ booking_system/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TableBooking
 
 # Register your models here.
+admin.site.register(TableBooking)
--- booking_system/models.py
+++ booking_system/models.py
@@ -3,3 +3,4 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class TableBooking(models.Model):
+    date = models.DateField() # Date the user wants to book
