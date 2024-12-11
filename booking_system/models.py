ad9f5d7e430bcedc411520248bde3a1a38d1e7a4
Ashley Goward <ashleygoward@icloud.com>
2024-12-11 12:21:48 +0000

    first model works, user can pick date for table

--- booking_system/models.py
+++ booking_system/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here. # UNDER CONSTRUCTION :D
 class TableBooking(models.Model):
     date = models.DateField() # Date the user wants to book
