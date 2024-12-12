42c85711e61ffb81d0320954bba0144952ac4509
Ashley Goward <ashleygoward@icloud.com>
2024-12-12 13:04:07 +0000

    link user to booking, delete booking if user is removed

--- booking_system/models.py
+++ booking_system/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import User
 # Create your models here. # UNDER CONSTRUCTION :D
 class TableBooking(models.Model):
     date = models.DateField() # Date the user wants to book
+    user = models.ForignKey(User, on_delete=models.CASCADE) # Links the booking to the user; deletes bookings if the user is removed
