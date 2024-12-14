74d829a6dc133681990add4b648199f930573727
Ashley Goward <ashleygoward@icloud.com>
2024-12-14 14:27:49 +0000

    revert import views for make booking

--- restaurant_booking_system/urls.py
+++ restaurant_booking_system/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from booking_system import views
 
 urlpatterns = [
-    path("", include("booking_system.urls")), 
+    path('make/', views.make_booking, name='make_booking'),
     path('admin/', admin.site.urls),
 ]
