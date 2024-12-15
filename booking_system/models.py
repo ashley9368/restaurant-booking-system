ba5493927f291dc32bd05d85a537fbadef40958f
Ashley Goward <ashleygoward@icloud.com>
2024-12-15 11:45:41 +0000

    add first time slot for test

--- booking_system/models.py
+++ booking_system/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here. # UNDER CONSTRUCTION :D
 class TableBooking(models.Model):
+    TIME_SLOTS = [
+        ('09:00', '9:00 AM')
+    ]
     date = models.DateField() # Date the user wants to book
     time = models.TimeField() # Time the user would like to book
     guests = models.PositiveIntegerField(default=1) # Sets the default number of guests to 1
