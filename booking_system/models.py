0e3ba57efaff534fee2e4800fd6051b40c8115ca
Ashley Goward <ashleygoward@icloud.com>
2024-12-15 10:50:22 +0000

    add a time field to TableBooking model

--- booking_system/models.py
+++ booking_system/models.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import User
 # Create your models here. # UNDER CONSTRUCTION :D
 class TableBooking(models.Model):
     date = models.DateField() # Date the user wants to book
+    time = models.TimeField() # Time the user would like to book
     user = models.ForeignKey(User, on_delete=models.CASCADE) # Links the booking to the user; deletes bookings if the user is removed
