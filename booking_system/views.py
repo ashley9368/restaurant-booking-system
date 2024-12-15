68f1b9d241f6467c1e099faf1444ee64b77b388d
Ashley Goward <ashleygoward@icloud.com>
2024-12-15 14:15:10 +0000

    link js and css

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def make_booking(request):
-    return HttpResponse("Make a Booking!")
+    return render(request, 'booking.html') # Path to booking.html in templates folder
--- restaurant_booking_system/settings.py
+++ restaurant_booking_system/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'restaurant_booking_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'], # Path to templates
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # Path to templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,6 +135,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
