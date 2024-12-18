8d13a28f1b1f47f7207825b0e56336cd82ed011a
Ashley Goward <ashleygoward@icloud.com>
2024-12-18 10:26:22 +0000

    add user first name, last, and email to table_booking

--- booking_system/models.py
+++ booking_system/models.py
@@ -22,3 +22,6 @@ class TableBooking(models.Model):
     time = models.CharField(max_length=5, choices=TIME_SLOTS) # Time the user would like to book
     guests = models.PositiveIntegerField(default=1) # Sets the default number of guests to 1
     user = models.ForeignKey(User, on_delete=models.CASCADE) # Links the booking to the user; deletes bookings if the user is removed
+    first_name = models.CharField(max_length=100, default="") # First name for the booking
+    last_name = models.CharField(max_length=100, default="") # Last name for the booking
+    email = models.EmailField(max_length=100, default="") # Email for customer
