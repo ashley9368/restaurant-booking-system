5578cb2464571ed6c7d2bd2c5ba33cfda8384562
Ashley Goward <ashleygoward@icloud.com>
2024-12-18 10:50:26 +0000

    add TableBookingForm for making bookings with django forms

--- booking_system/forms.py
+++ booking_system/forms.py
@@ -1,2 +1,11 @@
 from django import forms
 from .models import TableBooking
+
+class TableBookingForm(forms.ModelForm):
+    class Meta:
+        model = TableBooking
+        fields = ['date', 'time', 'guests', 'first_name', 'last_name', 'email']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'time': forms.Select(choices=TableBooking._meta.get_field('time').choices),
+        }
--- booking_system/views.py
+++ booking_system/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import TableBooking
 
 # Create your views here.
 def make_booking(request):
     return render(request, 'booking.html') # Path to booking.html in templates folder
+
+def user_bookings(request):
+    bookings = TableBooking.objects.filter(user=request.user) # Gets table booking data
+    return render(request, 'booking.html', {bookings})
