2a70b27f6b434f539b1ddd4fde141ef21f22496d
Ashley Goward <ashleygoward@icloud.com>
2024-12-18 09:37:08 +0000

    re structure models.py

--- booking_system/models.py
+++ booking_system/models.py
@@ -3,29 +3,21 @@ from django.contrib.auth.models import User
 
 # Monday - Sunday: 9:00 AM - 9:00 PM
 
-# Create your models here. # UNDER CONSTRUCTION :D 
+# Create your models here. # UNDER CONSTRUCTION :D
 
-#Times avaliable for booking                                
+#Times avaliable for booking
+TIME_SLOTS = [
+    ('12:00', '12:00 PM'),
+    ('13:00', '13:00 PM'),
+    ('14:00', '14:00 PM'),
+    ('15:00', '15:00 PM'),
+    ('16:00', '16:00 PM'),
+    ('17:00', '17:00 PM'),
+    ('18:00', '18:00 PM'),
+    ('19:00', '19:00 PM'),
+    ('20:00', '20:00 PM'),
+]
 class TableBooking(models.Model):
-    TIME_SLOTS = [
-        ('09:00', '9:00 AM'),
-        ('10:00', '10:00 AM'),
-        ('11:00', '11:00 AM'),
-        ('12:00', '12:00 PM'),
-        ('13:00', '13:00 PM'),
-        ('14:00', '14:00 PM'),
-        ('15:00', '15:00 PM'),
-        ('16:00', '16:00 PM'),
-        ('17:00', '17:00 PM'),
-        ('18:00', '18:00 PM'),
-        ('19:00', '19:00 PM'),
-        ('20:00', '20:00 PM'),
-    ]
-
-    TABLE_NUMBERS = [(i, f"Table {i} (2 seats)") for i in range (1,5)] + \
-                    [(i, f"Table {i} (4 seats)") for i in range(5, 11)] + \
-                    [(11, "Table 11 (8 seats)")]
-
     date = models.DateField() # Date the user wants to book
     time = models.CharField(max_length=5, choices=TIME_SLOTS) # Time the user would like to book
     guests = models.PositiveIntegerField(default=1) # Sets the default number of guests to 1
