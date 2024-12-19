dd70fe6895e1005c66ae6596dee2fb73dda8f802
Ashley Goward <ashleygoward@icloud.com>
2024-12-19 12:26:05 +0000

    add signup form to signup page

--- customer_accounts/forms.py
+++ customer_accounts/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.contrib.auth.models import User # Imports built in user model
-from django.contrib.auth.forms import UserCreationFrom
+from django.contrib.auth.forms import UserCreationForm
 
 class SignupForm(UserCreationForm): # Extend UserCreationForm to add email field
     email = forms.EmailField(required=True) # Add email field and make it required
 
     class Meta:
         model = User # Use the built in user model
-        fields = ['Username', 'email', 'password1', 'password2'] # Specify the fields to display to the user
+        fields = ['username', 'email', 'password1', 'password2'] # Specify the fields to display to the user
--- customer_accounts/views.py
+++ customer_accounts/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from .forms import SignupForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
 # Check if the request method is POST (form submitted)
 def signup(request): 
     if request.method == 'POST':
-        form = SignupFrom(request.POST)
+        form = SignupForm(request.POST)
+# Validate the form and save the data if its valid
+        if form.is_valid(): 
+            form.save()
+            # redirects user to login page
+            return redirect('login')
+    else: 
+        form = SignupForm()
+# Load the signup.html template with the form
+    return render(request, 'signup.html', {'form': form})
