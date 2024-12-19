d72caf17777d0cbb79c2f0f55f440b0c8fe2cd29
Ashley Goward <ashleygoward@icloud.com>
2024-12-19 09:43:30 +0000

    add form wrapper in booking.html for styling

--- booking_system/views.py
+++ booking_system/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from .forms import TableBookingForm
 
 # Create your views here.
+
+# Check if the request method is POST (form submitted)
 def make_booking(request):
     if request.method == 'POST':
         form = TableBookingForm(request.POST)
-
+# Validate the form and save the data if its valid
         if form.is_valid():
             form.save()
-
-        else: form = TableBookingForm()
-
+# If its not a POST request display empty form
+    else: form = TableBookingForm()
+# Load the booking.html template with the form
     return render(request, 'booking.html', {'form': form})
