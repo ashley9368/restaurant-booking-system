60cb5c4168982143f74bd81cda0551ab79193047
Ashley Goward <ashleygoward@icloud.com>
2024-12-19 09:20:56 +0000

    Add make_booking view with post handling

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from .models import TableBooking
+from .forms import TableBookingForm
 
 # Create your views here.
 def make_booking(request):
-    return render(request, 'booking.html') # Path to booking.html in templates folder
+    if request.method == 'POST':
+        form = TableBookingForm(request.POST)
 
-def user_bookings(request):
-    bookings = TableBooking.objects.filter(user=request.user) # Gets table booking data
-    return render(request, 'booking.html', {bookings})
+        if form.is_valid():
+            form.save()
+
+    else: form = TableBookingForm()
+
+    return render(request, 'booking.html', {'form': form})
