a1d741982e7ccf7cd31effbd8d9d918afdc9ba47
Ashley Goward <ashleygoward@icloud.com>
2024-12-20 13:43:58 +0000

    add login form

--- customer_accounts/forms.py
+++ customer_accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User # Imports built in user model
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 class SignupForm(UserCreationForm): # Extend UserCreationForm to add email field
     email = forms.EmailField(required=True) # Add email field and make it required
@@ -8,3 +9,12 @@ class SignupForm(UserCreationForm): # Extend UserCreationForm to add email field
     class Meta:
         model = User # Use the built in user model
         fields = ['username', 'email', 'password1', 'password2'] # Specify the fields to display to the user
+
+class LoginForm(AuthenticationForm): # Extend django's built in AuthenticationForm for validation
+    username = forms.CharField(
+        max_length=150,
+        required=True,
+    )
+    password = forms.CharField(
+        required=True,
+    )
