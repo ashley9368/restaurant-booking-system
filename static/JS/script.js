906656419620c5ce84a64f080daa8d76f19cb9cb
Ashley Goward <ashleygoward@icloud.com>
2024-12-22 13:18:10 +0000

    expand on signup icon at top right, to become login/logout/signup area, create js to make menu toggle to see options, rearrange html and re style css

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,7 +1,15 @@
 // This function toggles the visibility of the dropdown menu when the button is clicked
 
+/* Dropdown menu for users to navigate */ 
 function toggleMenu() {
     var menu = document.getElementById("dropdownList");
     // Toggle the 'visible' class to show or hide the menu
     menu.classList.toggle("visible");
 };
+
+/* Dropdown menu for users to see account status/login/signup */ 
+function toggleAccountMenu() {
+    var menu = document.getElementById("account-dropdown");
+    // Toggle the 'visible' class to show or hide the menu
+    menu.classList.toggle("visible");
+}
