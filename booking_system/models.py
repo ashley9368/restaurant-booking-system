de0cf8939bbbdfc4c8c99b691e6b7d3cfb7867a9
Ashley Goward <ashleygoward@icloud.com>
2025-01-06 16:14:37 +0000

    Create table model with table number, capacity and status, and return a string in admin interface for better readability

--- booking_system/models.py
+++ booking_system/models.py
@@ -18,6 +18,20 @@ TIME_SLOTS = [
     ('20:00', '20:00 PM'),
 ]
 
+# This model represents a table in a restaurant 
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True) # Unique number for the table
+    capacity = models.IntegerField() # How many guests the table can seat
+    status = models.CharField(
+        max_length=10,
+        choices=[('available', 'Available'), ('booked', 'Booked')],
+        default='available' # A status weather its booked or available
+    )
+
+    # Provides better readability for the admin interface instead of a random reference
+    def __str__(self):
+        return f"Table {self.table_number} ({self.capacity} seats)"
+
 class TableBooking(models.Model):
     date = models.DateField() # Date the user wants to book
     time = models.CharField(max_length=5, choices=TIME_SLOTS) # Time the user would like to book
@@ -26,3 +40,8 @@ class TableBooking(models.Model):
     first_name = models.CharField(max_length=100, default="") # First name for the booking
     last_name = models.CharField(max_length=100, default="") # Last name for the booking
     email = models.EmailField(max_length=100, default="") # Email for customer
+    tables = models.ManyToManyField(Table) # link multiple tables to a booking
+
+    # Provides better readability for the admin interface instead of a random reference
+    def __str__(self):
+        return f"Booking by {self.first_name} {self.last_name} for {self.guests} guests"
