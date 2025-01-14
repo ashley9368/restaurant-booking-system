67b6f25e231076709439c98ab7124b1c83d5c6d5
Ashley Goward <ashleygoward@icloud.com>
2025-01-14 13:45:44 +0000

    Change guest field to a drop down of choices in form

--- booking_system/forms.py
+++ booking_system/forms.py
@@ -6,6 +6,24 @@ class TableBookingForm(forms.ModelForm):
         model = TableBooking
         fields = ['date', 'time', 'guests', 'first_name', 'last_name', 'email']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
+            'date': forms.DateInput(attrs={'type': 'date'}), 
             'time': forms.Select(choices=TableBooking._meta.get_field('time').choices),
         }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Change the 'guests' field to a dropdown
+        self.fields['guests'] = forms.ChoiceField(
+            choices= [ 
+                (1, '1'),
+                (2, '2'),
+                (3, '3'),
+                (4, '4'),
+                (5, '5'),
+                (6, '6'),
+                (7, '7'),
+                (8, '8'),
+            ], 
+            label="Guests" #
+        )
